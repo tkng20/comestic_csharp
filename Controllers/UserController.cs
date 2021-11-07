61a9d09fb276e5dc28156f130d18caca3118617c
yin2002 <kimngan200173@gmail.com>
2021-11-08 01:06:10 +0700

    gudnight

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
 using comestic.Models;
 
 namespace comestic.Controllers
@@ -33,14 +32,14 @@ namespace comestic.Controllers
                 return NotFound();
             }
 
-            var userModel = await _context.Users
+            var users = await _context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (userModel == null)
+            if (users == null)
             {
                 return NotFound();
             }
 
-            return View(userModel);
+            return View(users);
         }
 
         // GET: User/Create
@@ -50,19 +49,19 @@ namespace comestic.Controllers
         }
 
         // POST: User/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Email,Password,Photo,Role,Provider,ProviderId,Status,RememberToken,CreatedAt,UpdatedAt")] Users userModel)
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Password,Photo,Role,Status,RememberToken,Created_at,Updated_at")] Users users)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(userModel);
+                _context.Add(users);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(userModel);
+            return View(users);
         }
 
         // GET: User/Edit/5
@@ -73,22 +72,22 @@ namespace comestic.Controllers
                 return NotFound();
             }
 
-            var userModel = await _context.Users.FindAsync(id);
-            if (userModel == null)
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
             {
                 return NotFound();
             }
-            return View(userModel);
+            return View(users);
         }
 
         // POST: User/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Email,Password,Photo,Role,Provider,ProviderId,Status,RememberToken,CreatedAt,UpdatedAt")] Users userModel)
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Email,Password,Photo,Role,Status,RememberToken,Created_at,Updated_at")] Users users)
         {
-            if (id != userModel.Id)
+            if (id != users.Id)
             {
                 return NotFound();
             }
@@ -97,12 +96,12 @@ namespace comestic.Controllers
             {
                 try
                 {
-                    _context.Update(userModel);
+                    _context.Update(users);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!UserModelExists(userModel.Id))
+                    if (!UsersExists(users.Id))
                     {
                         return NotFound();
                     }
@@ -113,7 +112,7 @@ namespace comestic.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(userModel);
+            return View(users);
         }
 
         // GET: User/Delete/5
@@ -124,14 +123,14 @@ namespace comestic.Controllers
                 return NotFound();
             }
 
-            var userModel = await _context.Users
+            var users = await _context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (userModel == null)
+            if (users == null)
             {
                 return NotFound();
             }
 
-            return View(userModel);
+            return View(users);
         }
 
         // POST: User/Delete/5
@@ -139,13 +138,13 @@ namespace comestic.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
-            var userModel = await _context.Users.FindAsync(id);
-            _context.Users.Remove(userModel);
+            var users = await _context.Users.FindAsync(id);
+            _context.Users.Remove(users);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool UserModelExists(long id)
+        private bool UsersExists(long id)
         {
             return _context.Users.Any(e => e.Id == id);
         }
